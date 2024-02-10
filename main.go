571ce6546ab48df78eb5f6cb291977bddb2b930f
AbrLis <abrli@yandex.ru>
2024-02-10 13:32:00 +0500

    Добавлено логирование в файл запуска системы

--- main.go
+++ main.go
@@ -14,13 +14,18 @@ func main() {
 	orchestrator := apiEndpoint.NewOrchestrator(db)
 
 	// Запуск API-оркестратора
+	log.Println("Попытка запуска API-оркестратора")
 	err := orchestrator.Run() // Запуск определён по адресу localhost:3000
 	if err != nil {
 		log.Println("Ошибка запуска API-оркестратора")
 		panic(err)
 	}
+	log.Printf("API-оркестратор запущен по адресу http://%s:%s\n", apiEndpoint.HostPath, apiEndpoint.PortHost)
 
 	calculators := agent.NewFreeCalculators()
 	calculators.RunCalculators() // Запуск вычислительных операций
+	log.Println("Вычислители запущены")
 	agent.RunDeamon(calculators) // Запуск демона вычислителя
+	log.Println("Демон вычислитетей запущен")
+	log.Println("Система готова к использованию")
 }
